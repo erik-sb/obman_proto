f2d45e3bf6b5c079fc422b5391adcfe4e2817d44
Erik Nelson <erik@strayboots.com>
2014-11-18 18:06:51 -0500

    Clean up map ojbect manager as a result of our convo

--- MapObjectManager.js
+++ MapObjectManager.js
@@ -1,17 +1,8 @@
 var Promise = require('rsvp').Promise;
 var Map = require('./Map');
 
-var states = {
-	CLOSED: 0,
-	OPEN: 1
-}
-
 function _randomMap(){
 	return Map("Map:" + Math.floor(Math.random() * 100));
-}
-
-function _stateToContextName(state){
-	return state === states.CLOSED ? "RK Context" : "Private Context";
 }
 
 var mockDataClient = {
@@ -39,56 +30,19 @@ var mockDataClient = {
 function MapObjectManager(){
 	var o = {};
 
-	o._context = null;
-	o._currentState = states.CLOSED;
-
-	//////////////////////////////
-	// TRANSACTIONS
-	//////////////////////////////
-	o.startTransaction = function(){
-		if(!this._context){
-			console.log("Opening new transaction");
-
-			o._context = [];
-
-		} else {
-			console.log("WARNING: Attempted to open a new transaction when one is already open. Returning null.");
-			return null;
-		}
-	}
-
-	o.abortTransaction = function(){
-		if(o._currentState = states.OPEN){
-			console.log("WARNING: No open transactions to abort.")
-		} else {
-			console.log("Aborting open transaction");
-			o._context = null;
-			o._currentState = states.CLOSED;
-		}
-	}
-
-	o.commitTransaction = function(){
-		return Promise(function(resolve, reject){
-
-			// Mocking some random work that may succeed or fail
-			mockDataClient.randomAsyncWork(function(didSucceed){
-
-				if(didSucceed){
-					resolve(_randomMap());
-				} else {
-					reject("No network cx");
-				}
-
-			})
-
-		});
-	}
-
 	//////////////////////////////
 	// LOCAL CRUD
 	//////////////////////////////
-	o.createMap = function(name){
-		console.log("Creating context in " + _stateToContext(this._currentState));
+	o.createMap = function(name, tx){
+		if(tx){
+			console.log("createMap called with tx");
+			console.log("createdMap as part of context:", tx.getContext());
+
+			tx.addOperation("Created map " + name);
+		} else {
+			console.log("createMap called without a tx, defaulting to RK context");
+		}
+
 		return Map(name);
 	};
 
@@ -140,7 +94,7 @@ function MapObjectManager(){
 		});
 	}
 
+	return o;
 }
 
 exports.create = function (){ return MapObjectManager(); };
-exports.states = states;
