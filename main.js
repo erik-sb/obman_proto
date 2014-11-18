995892dbd6802262ea4b68c87be438a7299805a1
Erik Nelson <erik@strayboots.com>
2014-11-18 18:07:53 -0500

    Add some logging

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var viewController = require('./ViewController')();
 
+console.log();
+console.log("==================================================");
+console.log(" Start ObjectManagerProto...");
+console.log("==================================================");
+console.log();
 viewController.init();
 viewController.buttonPress();
